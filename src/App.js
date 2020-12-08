f89f34d6567ddca5d5a34b8c23c52e541f5e08f8
jtreeves <jackson.t.reeves@gmail.com>
2020-12-08 11:44:38 -0500

    [App] Add props to display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <div className="App">
+            <h1>{props.post.title}</h1>
+            <p>by {props.post.author}</p>
+            <p>{props.post.body}</p>
+            <h2>Comments:</h2>
+            <p>{props.post.comments[0]}</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
